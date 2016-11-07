a3efda2c1cf5381ffc4c1612ca5969cfd5283ef4
Gabriele Merli <gabrielemerli@yahoo.it>
2016-11-07 15:49:25 +0100

    Add sqlite for POST

---
+++ db.js
@@ -0,0 +1,19 @@
+//Load module and connect to db (tipo config.inc.php)
+
+var Sequelize = require('sequelize');
+//Nuova istanza di sequelize (classe -> oggetto)
+var sequelize = new Sequelize(undefined, undefined, undefined, {
+	'dialect': 'sqlite',
+	'storage': __dirname + '/data/dev-todo-api.sqlite'
+});
+
+var db = {};
+
+//Richiama i modelli (le tabelle) di sqlite da file diversi
+db.todo = sequelize.import(__dirname + '/models/todo.js');
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+
+//Questo oggetto db ha i nostri modelli, un'istanza di sequelize, e le librerie di Sequelize
+module.exports = db;
---
+++ models/todo.js
@@ -0,0 +1,21 @@
+//Funzione speciale, gli viene passata una istanzza di sequelize e i datatypes (Sequelize)
+module.exports = function(sequelize, DataTypes) {
+
+	return sequelize.define('todo', {
+		description: {
+			type: DataTypes.STRING,
+			//Ora mettiamo un po di validation
+			allowNull: false,
+			validate: {
+				//Valida solo se la dimenzione della stringa è > 1 e minore di 250
+				len: [1, 250]
+			}
+		},
+		completed: {
+			type: DataTypes.BOOLEAN,
+			allowNull: false,
+			defaultValue: false
+		}
+	});
+
+};
