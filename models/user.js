fd5eb8eda616138143d2008327e63b9b1226ee19
Gabriele Merli <gabrielemerli@yahoo.it>
2016-11-10 13:03:30 +0100

    Add login route and authenticate class method

--- models/user.js
+++ models/user.js
@@ -3,7 +3,7 @@ var _ = require('underscore');
 
 
 module.exports = function(sequelize, DataTypes) {
-	return sequelize.define('user', {
+	var user = sequelize.define('user', {
 		email: {
 			type: DataTypes.STRING,
 			allowNull: false,
@@ -54,6 +54,42 @@ module.exports = function(sequelize, DataTypes) {
 				var json = this.toJSON();
 				return _.pick(json, 'id', 'email', 'updatedAt', 'createdAt');
 			}
+		},
+		classMethods: {
+			authenticate: function(body) {
+				return new Promise(function(resolve, reject) {
+
+					if ((typeof body.email !== 'string') || (typeof body.password !== 'string')) {
+						return reject();
+					}
+
+					//res.status(200).json(body);
+
+					//Cerco l'utente il cui indirizzo email è quello che mi arriva in post
+					user.findOne({
+						where: {
+							email: body.email
+						}
+					}).then(function(user) {
+						//User è un oggetto json, non un array perchè findOne ne trova SEMPRE uno (o zero)
+						if (!user || !bcrypt.compareSync(body.password, user.password_hash)) {
+							//Lui fa così, con il .get if (!user || !bcrypt.compareSync(body.password,user.get('password_hash'))) {
+							//SE non lo trovo (fermo l'esecuzione, perchè non uso else e quindi mi devo fermare)
+							//Oppure se la password non è quella buona
+							//401 -> l'autenticazione è possibile ma fallisce
+							return reject();
+						}
+						//Se lo trovo e la password è quella buona devo creare un token di autenticazione
+						resolve(user);
+
+					}, function(e) {
+						//In caso di errore
+						reject();
+					});
+				});
+			}
+
 		}
 	});
-}
+	return user;
+};
